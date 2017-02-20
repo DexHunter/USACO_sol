6b8343f45e9a92067d0e9db4a59582b94644641a
Dex Hunter <dixingxu@gmail.com>
2017-02-20 20:56:06 +0800

    rewrite

--- codeforces/t_square.c
+++ codeforces/t_square.c
@@ -1,27 +1,24 @@
+/* solution to 1a */
+
 #include<stdio.h>
 #include<math.h>
-long long int row,column,capture_size;
-void total_stone()
-{
-    long long int c=0;
-    long long int remaining_column=column;
-    if(capture_size==1)printf("%I64d\n",row*column);
-    else
-    {
-        while(remaining_column>0)
-        {
-            c++;
-            remaining_column-=capture_size;
-        }
- 
-        printf("%I64d\n",((long long int)(ceil)((double)row/capture_size))*c);
-    }
-}
+
+long long int row, col, a;
+
 int main()
 {
     while(scanf("%I64d %I64d %I64d",&row,&column,&capture_size)!=EOF)
     {
-        total_stone();
+        int ctr = 0;
+		long long int left_col = col;
+		if (a == 1) printf("I64d\n", row*col);
+		else{
+			while(left_col>0){
+				c++;
+				left_col-=a;
+			}
+			printf("I64d\n", ((long long int)(ceil)((double)row/a)) * c );
+		}
     }
     return 0;
 }
