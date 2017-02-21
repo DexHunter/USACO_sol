9179a010dcba1044dce18369464629cc364b48f4
Dex Hunter <dixingxu@gmail.com>
2017-02-21 11:33:18 +0800

    rewrite the code

--- codeforces/t_square.c
+++ codeforces/t_square.c
@@ -7,7 +7,7 @@ long long int row, col, a;
 
 int main()
 {
-    while(scanf("%I64d %I64d %I64d",&row,&col,&s)!=EOF)
+    while(scanf("%I64d %I64d %I64d",&row,&col,&a)!=EOF)
     {
         int ctr = 0;
 		long long int left_col = col;
